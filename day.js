b338f38338e5e7cf37a793cd50e96334eaea2886
ancevinz <ance.vincent@gmail.com>
2017-07-26 12:33:35 +0530

    Display the current day

---
+++ day.js
@@ -0,0 +1,4 @@
+var today = new Date()
+var day = today.getDay()
+var daylist = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+console.log('today is : ' + daylist[day] + '.')
