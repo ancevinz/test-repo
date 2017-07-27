ae4a9f8e49db42e60258d234756268bcb16477bc
ancevinz <30430496+ancevinz@users.noreply.github.com>
2017-07-27 11:42:23 +0530

    Revert "Minor updates in readme and files (#1)"
    This reverts commit fc46dd483b950925429eab13ecad11d88ea27956.

--- day.js
+++ day.js
@@ -1,4 +1,4 @@
 var today = new Date()
 var day = today.getDay()
 var daylist = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-console.log('today is: ' + daylist[day] + '.')
+console.log('today is : ' + daylist[day] + '.')
