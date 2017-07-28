5eae70324d33f6c497fbae4740d77af305b60886
ancevinz <ance.vincent@gmail.com>
2017-07-28 10:50:29 +0530

    Capitalize first letter in given string

---
+++ capitalize.js
@@ -0,0 +1,9 @@
+function capital_letter(str)
+{
+str = str.split(" ");
+for (var i = 0, x = str.length; i < x; i++) {
+str[i] = str[i][0].toUpperCase() + str[i].substr(1)
+}
+return str.join('')
+}
+console.log(capital_letter('capitalize the first letter of each word of a given string'))
