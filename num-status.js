5198ab091d284ca823daf17ac9b776b39c192c0c
ancevinz <ance.vincent@gmail.com>
2017-07-28 11:17:20 +0530

    Check one is positive and one is negative

---
+++ num-status.js
@@ -0,0 +1,11 @@
+function positive_negative (x, y) {
+  if ((x < 0 && y > 0) || x > 0 && y < 0) {
+    return true
+  } else {
+    return false
+  }
+}
+console.log(positive_negative(-5, 2))
+console.log(positive_negative(-2, -2))
+console.log(positive_negative(2, -8))
+console.log(positive_negative(-9, -41))
